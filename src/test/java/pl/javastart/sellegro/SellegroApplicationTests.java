fb2a97aa4caa630689130501fcdaf2dbef743ae4
Mateusz Dec <mateusz.dec92@gmail.com>
2020-01-16 11:54:59 +0100

    Zadanie domowe 28

---
+++ src/test/java/pl/javastart/sellegro/SellegroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.sellegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellegroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
